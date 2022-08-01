ccf419f76c601abdedb7875f63917d7ec6dcb820
Achalvlog <90440409+Achalvlog@users.noreply.github.com>
2022-08-01 14:22:20 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+
+public class A {
+	private int data=40;
+	private void msg() {System.out.println("hello java");}
+
+	public class Simple{
+
+	public static void main(String[] args) {
+		A obj=new A();
+		System.out.println(obj.data);
+		obj.msg();
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+}
